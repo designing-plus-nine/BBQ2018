8638c639b005b55401a60a3de11d26fea4ccfb5f
Sota Nakajima <32546883+joker-tempest@users.noreply.github.com>
2018-07-29 15:34:01 +0900

    add: gulp tasks (#1)

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require("browser-sync");
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+
+const paths = {
+  'scss': './scss/',
+  'css': './css/',
+  'html': './pages/',
+}
+const sassOptions = {
+  outputStyle: 'nested'
+}
+const autoPrefixerOptions = [
+  'last 4 versions',
+  '> 1%',
+  '> 1% in JP',
+]
+
+gulp.task('default', [
+  'watch',
+  'scss',
+  'browser-sync',
+]);
+
+gulp.task('watch', function () {
+  gulp.watch(paths.scss + '**/*.scss', ['scss']);
+});
+
+gulp.task('scss', function () {
+  gulp.src(paths.scss + 'bootstrap.scss')
+    .pipe(plumber({
+      errorHandler: notify.onError('<%= error.message %>')
+    }))
+		.pipe(sass(sassOptions).on('error', sass.logError))
+		.pipe(autoprefixer(autoPrefixerOptions))
+		.pipe(gulp.dest(paths.css));
+});
+
+gulp.task('browser-sync', () => {
+  browserSync({
+    server: {
+      baseDir: './',
+      index: paths.html + 'top.html'
+    }
+  });
+  // gulp.watch(paths.js + "**/*.js", ['reload']);
+  gulp.watch(paths.html + "**/*.html", ['reload']);
+  gulp.watch(paths.css + "**/*.css", ['reload']);
+});
+gulp.task('reload', () => {
+  browserSync.reload();
+});
